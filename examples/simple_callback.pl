76c7ce0c966bcc2ee4d55abe0a7e3f0f7bf32e0c
John Barrett <john@jbrt.org>
2025-09-08 15:30:45 +0100

    Add other devices to open call in examples

--- examples/simple_callback.pl
+++ examples/simple_callback.pl
@@ -6,7 +6,7 @@ use v5.36;
 use MIDI::RtMidi::FFI::Device;
 
 my $midi_in = RtMidiIn->new;
-$midi_in->open_port_by_name( qr/LKMK3/i ); # LaunchKey Mk 3
+$midi_in->open_port_by_name( qr/loop|LKMK3/i ); # LaunchKey Mk 3
 
 $midi_in->set_callback_decoded(
     sub { say join "\n", ( $_[0], $_[2]->@* ) }
