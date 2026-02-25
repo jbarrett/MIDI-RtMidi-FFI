8ac6fb24c4949865af77a0de7abc52ee77015d21
John Barrett <john@jbrt.org>
2026-02-25 18:09:04 +0000

    Remove implementation from Future::IO example, decode received bytes

--- examples/async_futureio.pl
+++ examples/async_futureio.pl
@@ -2,31 +2,34 @@
 
 use v5.36;
 
-use IO::Async::Loop;
 use Future::IO;
-use Future::IO::Impl::IOAsync;
 use Future::AsyncAwait;
 use Time::HiRes qw/ time /;
 use MIDI::RtMidi::FFI::Device;
+use MIDI::Stream::Decoder;
 
-my $loop = IO::Async::Loop->new;
 my $midi_in = RtMidiIn->new();
-$midi_in->open_port_by_name( qr/sz|loop/i );
+$midi_in->open_port_by_name( qr/sz|lkmk3/i );
+my $fh = $midi_in->get_fh;
+
+my $decoder = MIDI::Stream::Decoder->new;
+$decoder->attach_callback( all => sub( $event ) {
+    say join ' ', $event->dt, $event->as_arrayref->@*
+} );
 
 async sub msg {
-    my $fh = $midi_in->get_fh;
     my $size = $midi_in->bufsize;
-    while ( my $bytes = await Future::IO->read( $fh, $size ) ) {
-        say unpack 'H*', $bytes;
+    while ( my $midi_bytes = await Future::IO->read( $fh, $size ) ) {
+        $decoder->decode( $midi_bytes );
     }
 }
 
 async sub tick {
     my $tick = 0;
     while ( 1 ) {
-        await Future::IO->alarm( time + 1 );
+        await Future::IO->alarm( time() + 1 );
         say "Tick " . $tick++;
     }
 }
 
-$loop->await_all( tick msg );
+Future->wait_all( tick, msg )->get;
